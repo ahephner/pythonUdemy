50e666f1d9b24b1bed4138b1a38ea46c1ef30acd
AJ Hephner <ajhephner178@gmail.com>
2018-02-01 19:24:56 -0500

    first

---
+++ demo.py
@@ -0,0 +1,14 @@
+#print("Hello!")
+#print("AJ Hephner")
+
+#numbers in python
+
+#integer 3 whole numbers float 6.1 it's decimals 
+
+# ** is exponent 2 ** 3 = 8
+
+# % modulo like js 10 % 3 = 1 
+
+#// integer division 10//3 returns an int not a float
+
+ 
