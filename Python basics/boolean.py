083ccdeeef9097c98067e7fe9a042d3d84576791
AJ Hephner <ajhephner178@gmail.com>
2018-02-05 19:28:03 -0500

    added some boolean logic

---
+++ Python basics/boolean.py
@@ -0,0 +1,28 @@
+#elif is python for else if
+#inditation is very important 4 spaces
+# : means indented statement below
+# is same as == 
+
+
+print('Who is it?')
+name = input().upper()
+if name == "AJ": 
+    print("Hello"+ " "+ name)
+elif name == "Jackie":
+    print("naptime?")
+else:
+    print("why are you here?")
+    
+# 1 is a inherintly true
+#0 is false
+ # empty strings equal 0
+ #this is a way to evaluate for value
+
+animal = input('whats your favorite animal?')
+
+if animal:
+    print(animal + ' thats mine too!')
+else:
+    print('you didnt say an animal')
+
+    
---
+++ Python basics/comparisonoperators.py
@@ -0,0 +1,67 @@
+#== equal too
+1 == 1 #true
+a == A #false capitilize
+
+# != not equal too
+
+1 != 1 #false
+a != A #true
+
+#> or < greater than less than
+1 > 1 #false
+2> 1 #true
+
+#>= greater than or equal too <= less than or equal too 
+age = 18 
+
+if age >= 18: 
+    print('adult')
+else: 
+    print('your not an adult')
+
+#Logical Operators
+
+#and both have to be true
+dad = 60 
+mom = 55
+
+if mom >50 and dad > 50: 
+    print('time to retire')
+
+#or 1 has to be true
+
+city = input('where do you live')
+
+if city is 'indianapolis' or city is 'fort wayne':
+    print('Indiana')
+else: 
+    print('somewhere else')
+
+#not 
+
+#2-8 2 dollar ticket
+#65 + 5 dollar ticket
+#everyone else 10 dollar
+#gives you the opposite
+age = 21
+if not (age>= 2 and age<= 8) or age>=65):
+    print("10 dollars")
+else: 
+    print('discount')
+
+a = 1
+a == 1 #true
+a is 1 #true
+
+a = [1, 2]
+b = [1, 2]
+
+a== b  #true
+
+a is b #false is checks to see if their saved in the same 
+#place in the data and this a good way to find this out
+#[] are seperate objects in memory
+#== checks for values
+
+c = b
+b is c #true becuase they both point toward same data in memory
