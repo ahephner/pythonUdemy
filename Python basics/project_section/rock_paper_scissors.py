604501589e2427a1148d35a1629675c6abd9c799
AJ Hephner <ajhephner178@gmail.com>
2018-02-06 19:04:29 -0500

    added rock, paper, scissors

--- Python basics/project_section/rock_paper_scissors.py
+++ Python basics/project_section/rock_paper_scissors.py
@@ -1,20 +1,53 @@
+# print('...rock...')
+# print('...paper...')
+# print('...scissors...')
+
+# player1 = input('Player 1:')
+
+# # if player1 != "": 
+# print('NO CHEATING!!!!!!!!!')
+# print('NO CHEATING!!!!!!!!!')
+# print('NO CHEATING!!!!!!!!!')
+# print('NO CHEATING!!!!!!!!!')
+# print('NO CHEATING!!!!!!!!!')
+# print('NO CHEATING!!!!!!!!!')
+# print('NO CHEATING!!!!!!!!!')
+# print('NO CHEATING!!!!!!!!!')
+# print('NO CHEATING!!!!!!!!!')
+# player2 = input('Player 2: ')
+# if player1 == 'rock' and player2 == 'paper':
+#     print('player 2 wins')
+# elif player1 == 'paper' and player2 == 'scissors':
+#     print('player2 wins')
+# elif player1 == 'scissors' and player2 == 'rock':
+#     print('player2 wins')
+# elif player1 == 'rock' and player2 == 'scissors':
+#     print('player1 wins')
+# elif player1 == 'scissors' and player2 == 'paper':
+#     print('player1 wins')
+# elif player1 == 'paper' and player2 == 'rock':
+#     print('player1 wins')
+# elif player1 == player2:
+#     print('Tie game')
+# else: 
+#     print('flush')
+# # else:
+# #     print('Player 1 does not seem to want to play!')
+
+
+#with computer picking
+# random choose random could use random int
+import random 
+chance = ['paper', 'rock', 'scissors']
+
 print('...rock...')
 print('...paper...')
 print('...scissors...')
 
-player1 = input('Player 1:')
+player1 = input('Player 1:').lower()
+player2 = random.choice(chance)
 
-# if player1 != "": 
-print('NO CHEATING!!!!!!!!!')
-print('NO CHEATING!!!!!!!!!')
-print('NO CHEATING!!!!!!!!!')
-print('NO CHEATING!!!!!!!!!')
-print('NO CHEATING!!!!!!!!!')
-print('NO CHEATING!!!!!!!!!')
-print('NO CHEATING!!!!!!!!!')
-print('NO CHEATING!!!!!!!!!')
-print('NO CHEATING!!!!!!!!!')
-player2 = input('Player 2: ')
+print(f'computer says {player2}')
 if player1 == 'rock' and player2 == 'paper':
     print('player 2 wins')
 elif player1 == 'paper' and player2 == 'scissors':
