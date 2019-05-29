7608c92462378ce8148f0d594f3a0f9bfb1a8419
AJ Hephner <ajhephner178@gmail.com>
2019-05-29 16:16:43 -0400

    added inline table value function example

---
+++ Python basics/sql_section/advanced_tech.sql
@@ -0,0 +1,47 @@
+-- Create SumRideHrsSingleDay
+CREATE FUNCTION SumRideHrsSingleDay (@DateParm date)
+-- Specify return data type
+RETURNS numeric
+AS
+-- Begin
+BEGIN
+RETURN
+-- Add the difference between StartDate and EndDate
+(SELECT SUM(DATEDIFF(second, StartDate, EndDate))/3600
+FROM CapitalBikeShare
+ -- Only include transactions where StartDate = @DateParm
+WHERE Cast(StartDate AS date) = @DateParm)
+-- End
+END
+            
+--two inputs
+  -- Create the function
+CREATE FUNCTION SumRideHrsDateRange (@StartDateParm datetime, @EndDateParm datetime)
+-- Specify return data type
+RETURNS numeric
+AS
+BEGIN
+RETURN
+-- Sum the difference between StartDate and EndDate
+(Select SUM(DATEDIFF(second, StartDate, EndDate))/3600
+FROM CapitalBikeShare
+-- Include only the relevant transactions
+WHERE StartDate > @StartDateParm and StartDate < @EndDateParm)
+END
+/* INLINE TABLE VALUE FUNCTION */ 
+ -- Create the function
+CREATE FUNCTION SumStationStats(@StartDate AS datetime)
+-- Specify return data type
+RETURNS TABLE
+AS
+RETURN
+SELECT
+	StartStation,
+    -- Use COUNT() to select RideCount
+	COUNT(ID) AS RideCount,
+    -- Use SUM() to calculate TotalDuration
+    SUM(DURATION) AS TotalDuration
+FROM CapitalBikeShare
+WHERE CAST(StartDate as Date) = @StartDate
+-- Group by StartStation
+Group by StartStation;
