79d5d2e6f6b1d6c84f6da9dec81d0cf925161fa2
AJ Hephner <ajhephner178@gmail.com>
2019-06-17 14:40:54 -0400

    Updated a couple appends

--- Python basics/produced_docs/customer_goals_actual_sales.py
+++ Python basics/produced_docs/customer_goals_actual_sales.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+#This job is more suited to run in jupyter for easier debuging/checking
+#Jupyter file 'Testing Customer Goal Duplicates' 
 #static reps salesforce id...had to use workbench to get
 rep_id = pd.read_csv(r"C:\Users\AJ Hephner\Documents\2019\All Customer Goals\Sales Reps Salesforce ID.csv")
 doc = pd.read_csv(r"C:\Users\AJ Hephner\Documents\2019\All Customer Goals\Sales Detail 5_15.csv")
@@ -7,14 +8,18 @@ goals = pd.read_csv(r"C:\Users\AJ Hephner\Documents\2019\All Customer Goals\Cust
 
 # If you need to append a downloaded excel file do it here. I.E. if you ran all sales last month 
 # and then run it just from that date through today please add the downloaded file here    
-#download= pd.read_csv(rDOWNLOADED FILE HERE)
+download= pd.read_csv(r'C:\Users\AJ Hephner\Downloads\report1560357122480.csv')
 
 goals = goals.fillna(0)
 
 #If you are adding onto the doc file instead of running the entire year uncomment here
-#doc = doc.append(download)
+doc = download.append(doc, sort=False)
 
-doc = doc.drop_duplicates(subset='Sales doc Detail ID', keep='first')
+x = doc['Sales Doc Detail ID'].duplicated()
+#print('********here are duplicated entry********')
+#print(doc[x].head())
+
+doc = doc.drop_duplicates(subset='Sales Doc Detail ID', keep='first')
 doc = doc.merge(rep_id, on='Sales Rep (Doc): Sales Rep')
 #Real Account ID is a formula field on the doc
 df = doc[['Extended Price', 'Real Account ID', 'Id']]
@@ -25,12 +30,14 @@ df = df.rename(columns={'Real Account ID': 'Customer', 'Id': 'Sales Rep'})
 
 merged = df.merge(goals, on=['Customer', 'Sales Rep'])
 #this is to set up for future uploads so we don't have to upsert as many records
-merged['Total Sales'] = merged['Extended Price'] 
+merged['Actual FY Sales'] = merged['Extended Price'] 
 
-out = merged[['Record ID', 'Extended Price']]
+out = merged[['Record ID', 'Actual FY Sales']]
+#print('here is merge')
+print(merged.head())
 #bye
 out.to_csv(r"C:\Users\AJ Hephner\Documents\2019\All Customer Goals\landing.csv", index=False)
 
 #i like to save a copy of what the last upload was for review
-snapshot = merged
-snapshot.to_csv(r"C:\Users\AJ Hephner\Documents\2019\All Customer Goals\lastRun_snapshot.csv", index=False)
+#snapshot = merged
+#snapshot.to_csv(r"C:\Users\AJ Hephner\Documents\2019\All Customer Goals\lastRun_snapshot.csv", index=False)
